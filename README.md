ea1b4c5606ff46b61f4990a6e5272dd9d7940f6d
jvgranados <jgranadosflores@gmail.com>
2018-06-10 18:09:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+## React sample
+
+Sample application built with React
+
+[![JavaScript Style Guide](https://cdn.rawgit.com/standard/standard/master/badge.svg)](https://github.com/standard/standard)
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <header>
+          <h1>React Sample</h1>
+        </header>
+        <p>
+          Hello world!
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
